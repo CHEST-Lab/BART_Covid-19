e640a46465e14ed70aca1863964345b23265797d
Weemaan <bastibrinkmann94@gmail.com>
2020-05-02 16:48:32 +0200

    Added visualization

--- Age_Estimation.R
+++ Age_Estimation.R
@@ -7,9 +7,9 @@
 # Date:   20.04.2020
 #
 #
-# Note: Here we provide a the method used for the age interpolation. The RKI used different age groups than those
-#       provided by the census data from INKAR. Therefore a probabilistic age interpolation has been conducted.
-#       This is based on the assumption, that the distribution within the specific age groups is linear (see plot in line 72).
+# Note: Here we provide a the method used for the age estimation. The RKI used different age groups than those
+#       provided by the census data from INKAR. Therefore a probabilistic age estimation has been conducted.
+#       This is based on the assumption, of equal distribution within the specific age groups (see plot in line 64).
 
 
 library(dplyr)
@@ -38,17 +38,7 @@ for (this_row in 1:nrow(rki)) {
   # Select current Landkreis and convert to vector
   this_rki <- rki[this_row,1:11] %>%
     unlist()
-  
-  # Plot original INKAR age distribution
-  'barplot((this_rki[1:10]/100 * this_rki[11]), 
-          names = c("0-2", "3-5", "6-17", "18-24", "15-29", "30-49", "50-64", "65-74", "75-84", "84+"), 
-          xlab = "", 
-          ylab = "Population", 
-          main = "INKAR Age Distribution", 
-          col = "#69b3a2",
-          las=2)'
-  
-  
+
   #  Create sampels from old age groups:
   # x = ages per groupe
   # size = y*1000
@@ -64,13 +54,14 @@ for (this_row in 1:nrow(rki)) {
   A_75_84 <- sample(seq(75,84,1), size = this_rki[9]*10000, replace = T, prob = rep(1/10,10))
   A_85_100 <- sample(seq(84,100,1), size = this_rki[10]*10000, replace = T, prob = rep(1/17,17))
   
-  age_tests <- c(A_0_2, A_3_5, A_6_17, A_18_24, A_25_29, A_30_49, A_50_64, A_65_74, A_75_84, A_85_100) # combine all tests
+  #Combine all samples
+  age_sampeles <- c(A_0_2, A_3_5, A_6_17, A_18_24, A_25_29, A_30_49, A_50_64, A_65_74, A_75_84, A_85_100)
   
   # Compute Empirical Cumulative Distribution Function
   P <- ecdf(age_tests)
   
   # Plot cumulated probabilities
-  #plot(P, main = "Empirical Cumulative Distribution Function", xlab = "Age", ylab = "Cumulative Probability")
+  #plot(P, main = "Empirical Cumulative Distribution Function", xlab = "Age", ylab = "Cumulative Probability", verticals = T, do.points = F)
   
   # Create probabilities for new age groups
   New_A_0_4 <-  P(4)
@@ -83,14 +74,6 @@ for (this_row in 1:nrow(rki)) {
   # Multiply the probability by population to calculate absolute population per age-group
   age_groups <- c(New_A_0_4, New_A_5_14, New_A_15_34, New_A_35_59, New_A_60_79, New_A_80_100) * this_rki[11]
   
-  # Plot population per age-group
-  'barplot(age_groups, 
-          names=c("0-4", "5-14", "15-34", "35-59", "60-79", "80+"), 
-          xlab = "", 
-          ylab = "Population", 
-          main = "Estimated RKI Age Distribution", 
-          col="#69b3a2",
-          las=2)'
   
   # Include NUTS2 code and results to output
   output[this_row,1] <- rki[this_row,12]
@@ -99,6 +82,41 @@ for (this_row in 1:nrow(rki)) {
   
   setTxtProgressBar(pr, this_row)
 }
+
+# Plot Age Comparison
+par(mfrow = c(1,2))
+rki %>% 
+  mutate("0-2" = S_026/100 * EWZ,
+         "3-5" = S_027/100 * EWZ,
+         "6-17" = S_029/100 * EWZ,
+         "18-24" = S_030/100 * EWZ,
+         "25-29" = S_031/100 * EWZ,
+         "30-49" = S_032/100 * EWZ,
+         "50-64" = S_033/100 * EWZ,
+         "65-74" = S_034/100 * EWZ,
+         "75-84" = S_037/100 * EWZ,
+         "84+" = S_038/100 * EWZ) %>% 
+  select(c("0-2", "3-5", "6-17", "18-24", "25-29", "30-49", "50-64", "65-74", "75-84", "84+")) %>% 
+  colSums() %>%  
+  barplot(names = c("0-2", "3-5", "6-17", "18-24", "25-29", "30-49", "50-64", "65-74", "75-84", "84+"), 
+          xlab = "", 
+          ylab = "Population in millions", 
+          main = "INKAR Age Distribution", 
+          col = "#69b3a2",
+          las=2,
+          yaxt = "n")
+axis(2, at = seq(0, 2.0e+07, len=5), labels = seq(0, 2.0e+07, len=5)/1000000)
+  
+output[,2:7] %>% 
+  colSums() %>% 
+  barplot(names=c("0-4", "5-14", "15-34", "35-59", "60-79", "80+"), 
+          xlab = "", 
+          ylab = "Population in millions", 
+          main = "Estimated RKI Age Distribution", 
+          col="#69b3a2",
+          las=2,
+          yaxt = "n")
+axis(2, at = seq(0, 2.5e+07, len=6), labels = seq(0, 2.5e+07, len=6)/1000000)
 
 # Safe as .csv
 write.csv(output, "Data/Age_RKI.csv", row.names = F)
